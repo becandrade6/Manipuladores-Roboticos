29c8ea3229420260947f77a913dbb084ab8a786c
Bernardo Andrade <bernardo.andrade@engenharia.ufjf.br>
2023-11-01 11:34:48 -0300

    Adicionando teste de funções demonstrativo

---
+++ Trabalho2/testeFunctions.py
@@ -0,0 +1,15 @@
+from ManipuladoresFunctions import *
+import numpy as np
+import sympy as sp
+
+#declarando simbólicos
+dx, dy, dz = sp.symbols('dx dy dz')
+
+# Teste de funções
+F0 = sp.eye(4)
+
+A1 = X_Sym_Translation(dx)*Z_Sym_Translation(dz)*Y_Sym_Translation(dy)
+
+F1 = F0 * A1
+
+sp.pprint(F1)
